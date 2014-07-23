2a097aabfeb739f1d84bfcbbfa67d84459c22331
testusername <testusername@test.com>
2014-07-23 14:32:53 +0200

    ltg

--- src/Kikala/FrontBundle/Controller/FormationController.php
+++ src/Kikala/FrontBundle/Controller/FormationController.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace kikala\FrontBundle\Controller;
+namespace Kikala\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class FormationController extends Controller
 {
 	public function lsFormaAction(){
-		return $this->render('kikalaFrontBundle:Formation:lsForma.html.twig');
+		return $this->render('KikalaFrontBundle:Formation:lsForma.html.twig');
 	}
 
 	public function formaDetailAction(){
-		return $this->render('kikalaFrontBundle:Formation:formaDetail.html.twig');
+		return $this->render('KikalaFrontBundle:Formation:formaDetail.html.twig');
 	}
 
 }
--- src/Kikala/FrontBundle/Controller/UserController.php
+++ src/Kikala/FrontBundle/Controller/UserController.php
@@ -13,6 +13,19 @@ class UserController extends Controller
 
     public function loginAction()
     {
+ 	      $request = $this->getRequest();
+      $session = $request->getSession();
+      // get the login error if there is one
+	     if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+          $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+      } else {
+          $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+          $session->remove(SecurityContext::AUTHENTICATION_ERROR);   
+             return $this->render('KikalaFrontBundle:User:login.html.twig', array(
+          // last username entered by the user
+          'last_username' => $session->get(SecurityContext::LAST_USERNAME),  
+          'error'         => $error,
+      ));
         return $this->render('KikalaFrontBundle:User:login.html.twig');
     }
 
