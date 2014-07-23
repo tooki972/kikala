e1dd844f8d170a6474dc206cb49ea998498a842b
aboss <manon_mialet@hotmail.fr>
2014-07-23 10:22:41 +0200

    page a propos

--- src/Kikala/FrontBundle/Controller/DefaultController.php
+++ src/Kikala/FrontBundle/Controller/DefaultController.php
@@ -6,8 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+	 public function indexAction()
     {
-        return $this->render('KikalaFrontBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('KikalaFrontBundle:Default:index.html.twig');
+    }
+    public function legalAction()
+    {
+        return $this->render('KikalaFrontBundle:Default:legal.html.twig');
+    }
+     public function aboutAction()
+    {
+        return $this->render('KikalaFrontBundle:Default:about.html.twig');
     }
 }
